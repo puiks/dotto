dec3abb2eafd127aa7d5ece7371bdc76d47d5df2
Colinnnnn <754969928@qq.com>
2026-03-29 23:35:18 +0800

    Revert "Prepare release v0.1.4"
    This reverts commit 6f2bb15d37dd064d6d7367ba5bd044b87a54d49a.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.dotto.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 9
-        versionName = "0.1.4"
+        versionCode = 8
+        versionName = "0.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
